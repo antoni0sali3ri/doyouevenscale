1a608af581c50b82de2f59af1b9618f31f1315ff
antoni0sali3ri <contact@theopensourceguy.de>
2020-10-11 00:53:38 +0200

    Add back action to DataManagerActivity

--- app/src/main/java/com/github/antoni0sali3ri/doyouevenscale/ui/activity/DataManagerActivity.kt
+++ app/src/main/java/com/github/antoni0sali3ri/doyouevenscale/ui/activity/DataManagerActivity.kt
@@ -7,10 +7,10 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
 import com.github.antoni0sali3ri.doyouevenscale.MyApp
 import com.github.antoni0sali3ri.doyouevenscale.R
 import com.github.antoni0sali3ri.doyouevenscale.ui.main.DataManagerPagerAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -24,6 +24,7 @@ class DataManagerActivity : AppCompatActivity() {
         super.onCreate(null)
         setContentView(R.layout.activity_data_manager)
         setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val sectionsPagerAdapter = DataManagerPagerAdapter(this, supportFragmentManager)
         viewPager = findViewById(R.id.view_pager)
@@ -38,6 +39,10 @@ class DataManagerActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            finish()
+            true
+        }
         R.id.action_create_entity -> {
             val position = viewPager.currentItem
             (viewPager.adapter as DataManagerPagerAdapter).getFragmentAt(position)?.createItem()
